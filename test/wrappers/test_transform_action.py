23071c4a4c3aea9c651dde5158f2279f23af85b2
Carlos Casas <ccasascuadrado@guidewire.com>
2020-05-27 17:46:35 +0200

    Improved coverage

--- test/wrappers/test_transform_action.py
+++ test/wrappers/test_transform_action.py
@@ -9,23 +9,34 @@ from stable_baselines.common.env_checker import check_env
 class TestTransformAction(unittest.TestCase):
     def test_init_reversed(self):
         env = self._create_env_cartpole_changed()
-        self.assertIsInstance(env, gym.ActionWrapper, "The environment has to inherit from gym.ActionWrapper")
+        self.assertIsInstance(env, gym.ActionWrapper,
+                              "The environment has to inherit from gym.ActionWrapper")
         check_env(env)
 
     def test_init_continuous(self):
         env = self._create_env_cartpole_continuous()
-        self.assertIsInstance(env, gym.ActionWrapper, "The environment has to inherit from gym.ActionWrapper")
+        self.assertIsInstance(env, gym.ActionWrapper,
+                              "The environment has to inherit from gym.ActionWrapper")
         check_env(env)
 
     def test_action_changed(self):
         env = self._create_env_cartpole_changed()
-        self.assertEqual(env.action(0), 1, "The action function for 0 is not being applied correctly")
-        self.assertEqual(env.action(1), 0, "The action function for 1 is not being applied correctly")
+        self.assertEqual(env.action(0), 1,
+                         "The action function for 0 is not being applied correctly")
+        self.assertEqual(env.action(1), 0,
+                         "The action function for 1 is not being applied correctly")
 
     def test_action_continuous(self):
         env = self._create_env_cartpole_continuous()
-        self.assertEqual(env.action([0.7]), 1, "The action function for 0.7 is not being applied correctly")
-        self.assertEqual(env.action([-0.5]), 0, "The action function for -0.5 is not being applied correctly")
+        self.assertEqual(env.action([0.7]), 1,
+                         "The action function for 0.7 is not being applied correctly")
+        self.assertEqual(env.action([-0.5]), 0,
+                         "The action function for -0.5 is not being applied correctly")
+
+    def test_reverse_action(self):
+        env = self._create_env_cartpole_continuous()
+        self.assertRaises(NotImplementedError, env.reverse_action([0.7]),
+                          "The reverse action function is not yet supported")
 
     @staticmethod
     def _create_env_cartpole_changed():
