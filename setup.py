02fd0609a1c8f5c0261890972529eddc7a43fe82
Carlos Casas <ccasascuadrado@guidewire.com>
2020-05-27 14:36:34 +0200

    Enhanced setup.py file

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
 
-def license():
-    with open('LICENSE.txt') as f:
-        return f.read()
-
-
 def readme():
     with open('README.md') as f:
         return f.read()
@@ -14,11 +9,15 @@ def readme():
 setup(name='gym-satellite-trajectory',
       version='0.0.1-snapshot',
       python_requires='>=3.6',
+      packages=['gym_satellite_trajectory'],
+      scripts=[],
       description='Gym environments for spacecraft trajectory optimization',
       long_description=readme(),
-      long_description_content_type="text/markdown",
+      long_description_content_type='text/markdown',
+      url='https://github.com/zampanteymedio/gym-satellite-trajectory',
       keywords='gym openai env environment ai reinforcement learning spacecraft satellite trajectory optimization',
-      license=license(),
+      license='Apache License 2.0',
+      author='Carlos M. Casas Cuadrado',
       author_email='carlos.marce@gmail.com',
       install_requires=['gym>=0.17.1',
                         'setuptools>=41.4.0',
