8a692179fc9b5066cc396c2c1c12291c87f61ce2
Carlos Casas <ccasascuadrado@guidewire.com>
2021-02-20 12:16:08 +0100

    Added tests for plotting

--- test/envs/test_perigee_raising_env.py
+++ test/envs/test_perigee_raising_env.py
@@ -1,5 +1,6 @@
 import unittest
 
+import matplotlib.pyplot as plt
 from stable_baselines3.common.env_checker import check_env
 
 from gym_satellite_trajectory.envs.perigee_raising_env import PerigeeRaisingEnv
@@ -14,4 +15,15 @@ class TestPerigeeRaisingEnv(unittest.TestCase):
 
     def test_render(self):
         env = PerigeeRaisingEnv()
-        env.render()
+        env.reset()
+        assert env.render() is None
+
+    def test_render_plot(self):
+        env = PerigeeRaisingEnv()
+        env.reset()
+        assert type(env.render('plot')) == plt.Figure
+
+    def test_render_prev_plot(self):
+        env = PerigeeRaisingEnv()
+        env.reset()
+        assert type(env.render('prev_plot')) == plt.Figure
