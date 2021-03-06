765ee9d2f95b7aa135d1c8ea6430a9e0673620bd
Carlos Casas <ccasascuadrado@guidewire.com>
2021-03-06 23:10:31 +0100

    Updated unit tests

--- test/test_envs.py
+++ test/test_envs.py
@@ -9,6 +9,10 @@ class TestEnvs(unittest.TestCase):
         env = gym.make("PerigeeRaising-Continuous3D-v0")
         check_env(env)
 
+    def test_create_env_perigeeraising_continuous3d_perturbed(self):
+        env = gym.make("PerigeeRaising-Continuous3D-v0", use_perturbations=True, perturb_action=True)
+        check_env(env)
+
     def test_create_env_perigeeraising_continuous1d(self):
         env = gym.make("PerigeeRaising-Continuous1D-v0")
         check_env(env)
